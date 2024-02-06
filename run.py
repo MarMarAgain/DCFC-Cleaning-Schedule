f122b4c39ae526fca67cfbab57654b0dfc570523
MarMarAgain <>
2024-02-06 23:06:47 +0000

    Have confirmed that I am now able to print code to google sheet via python  terminal

--- run.py
+++ run.py
@@ -7,14 +7,20 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-'Load Credentials and  Access spreadsheet' 
+# Load Credentials and Access spreadsheet 
 CREDS = Credentials.from_service_account_file('dcfccreds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-'Open the spreadsheet'
-try:
-    SHEET = GSPREAD_CLIENT.open('DCFC cleaning')
-    print("Successfully connected to Google Sheets.")
-    print("Spreadsheet title:", SHEET.title)
-    
+# Open the spreadsheet
+SHEET = GSPREAD_CLIENT.open('DCFC cleaning')
+print("Successfully connected to Google Sheets.")
+print("Spreadsheet title:", SHEET.title)
+
+# Select the worksheet
+worksheet = SHEET.sheet1
+
+# Insert data into the sheet
+data = ["Hello Again!"]
+worksheet.append_row(data)
+print("Data inserted successfully!")
