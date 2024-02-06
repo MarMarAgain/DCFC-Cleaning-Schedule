1304c5e0a3a70a5cd33de084893a55abe6363292
MarMarAgain <>
2024-02-06 22:54:35 +0000

    connected google sheet to python- fist pump-

--- run.py
+++ run.py
@@ -7,21 +7,14 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-# Load credentials from the file
+'Load Credentials and  Access spreadsheet' 
 CREDS = Credentials.from_service_account_file('dcfccreds.json')
-
-# Authorize the client using the credentials and scopes
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-# Open the spreadsheet
+'Open the spreadsheet'
 try:
     SHEET = GSPREAD_CLIENT.open('DCFC cleaning')
     print("Successfully connected to Google Sheets.")
     print("Spreadsheet title:", SHEET.title)
-except gspread.SpreadsheetNotFound:
-    print("The specified spreadsheet was not found.")
-except gspread.exceptions.APIError as e:
-    print("An error occurred while accessing Google Sheets API:", e)
-except Exception as e:
-    print("An unexpected error occurred:", e)
+    
